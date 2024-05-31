b602abe263a082cb77b88c1ca3b3ea25e09456c9
David Hodel <david.hodel@leuchterag.ch>
2024-05-31 14:00:47 +0200

    (64513) - add new sample project for parallel processing

---
+++ src/Extensions.Generic.Kafka.Hosting,ParallelProcessing/JobMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting.Kafka;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Extensions.Generic.Kafka.Hosting.ParallelProcessing;
+
+class JobMessageHandler : IMessageHandler<string, byte[]>
+{
+    readonly ILogger logger;
+    readonly Random random = new Random();
+
+    public JobMessageHandler(ILogger<JobMessageHandler> logger)
+    {
+        this.logger = logger;
+    }
+
+    public async Task Handle(string key, byte[] value)
+    {
+        var waitTime = random.Next(2000, 10000);
+
+        if (waitTime > 8000)
+        {
+            logger.LogInformation($"Simulating exception ({waitTime})");
+            throw new Exception($"Simulated exception ({waitTime})");
+        }
+
+        logger.LogInformation($"Received message from Kafka. Waiting {waitTime}ms");
+        await Task.Delay(waitTime); // Simulate processing time
+        logger.LogInformation($"Finished waiting {waitTime}ms");
+    }
+}
